e03f927696bb93266643420eed2ba46f93a46e3a
Ayedun Akindeji <akindejioluwaseun015@gmail.com>
2024-04-16 22:42:33 +0100

    feat : integrate Desciption scroll animations

---
+++ components/Wrappers/DescriptionWrapper/DescriptionWrapper.styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledDescriptionContainer = styled.div`
+  background-color: white;
+  position: fixed;
+  height: 100%;
+  width: 43%;
+  left: 0;
+  top: 0;
+  padding-top: 29vh;
+  overflow: hidden;
+`;
