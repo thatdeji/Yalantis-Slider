b70095e62fc36785979b88143b9916882c7f3ae8
Ayedun Akindeji <akindejioluwaseun015@gmail.com>
2024-04-16 22:42:52 +0100

    chore : init mock data

---
+++ utils/mock-data.ts
@@ -0,0 +1,37 @@
+export const houses = [
+  {
+    name: "Lagonia apartment",
+    priceInDollars: 998500,
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1713199588/1e33d39c118899c99e5366b8c8c997b0-min_exgczn.jpg",
+    mainTagline: "Buy an elite Real Estate",
+    secondaryTagline: "A Right Media Mix Can Make The Difference",
+    color: "#e96568",
+    textColor: "#ffffff",
+  },
+  {
+    name: "Glass blue house",
+    priceInDollars: 657000,
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1713199588/1e33d39c118899c99e5366b8c8c997b0-min_exgczn.jpg",
+    mainTagline: "Sleek House Side Style",
+    secondaryTagline: "Althusser Competing Interpolations And The Third Text",
+    color: "#da8455",
+    textColor: "#ffffff",
+  },
+  {
+    name: "Palazo Art-Apartment",
+    priceInDollars: 2345000,
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1713199588/1e33d39c118899c99e5366b8c8c997b0-min_exgczn.jpg",
+    mainTagline: "To Live Like An Artist",
+    secondaryTagline:
+      "To live with style, try a palatial experience of Palazzo Art - Apartment.",
+    color: "#e9e95f",
+    textColor: "#2f2c44",
+  },
+  // {
+  //     name: "",
+  //     priceInDollars: 657000,
+  //     img: "",
+  //     mainTagline: "",
+  //     secondaryTagline: ""
+  // }
+];
