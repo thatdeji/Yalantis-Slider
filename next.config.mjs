3601d5f62aae2d912b04ba8853e343418fe1f37d
Ayedun Akindeji <akindejioluwaseun015@gmail.com>
2024-04-16 11:03:37 +0100

    chore : load cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
