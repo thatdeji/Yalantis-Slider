3199a5d863ba4b3521fa310b4815aa0307fa32e1
Ayedun Akindeji <akindejioluwaseun015@gmail.com>
2024-04-17 17:30:30 +0100

    chore : update name

---
+++ components/Wrappers/BackgroundImageWrapper/BackgroundImageWrapper.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledBackgroundImageOuterWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 57%;
+  overflow: hidden;
+`;
+
+export const StyledBackgroundImageInnerWraper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: none;
+`;
