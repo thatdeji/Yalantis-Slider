a0ad355a47959d63f1c7213eb9c428777a031502
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-06-19 14:19:55 +0100

    feat : implement desktop mode

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import BackgroundImage from "$/components/BackgroundImage/BackgroundImage";
 import Description from "$/components/Description/Description";
 import Header from "$/components/Header/Header";
-import { useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import styles from "./page.module.css";
 import { StyledDescriptionContainer } from "$/components/Wrappers/DescriptionWrapper/DescriptionWrapper.styles";
 import { houses } from "$/utils/mock-data";
@@ -29,13 +29,20 @@ import {
 import NumberIndex from "$/components/NumberIndex/NumberIndex";
 import Preloader from "$/components/Preloader/Preloader";
 import { montserrat } from "./font";
+import useWindowWidth from "$/hooks/useWindowWidth";
+import {
+  StyledMobileAlertContainer,
+  StyledMobileAlertText,
+} from "$/components/MobileAlert/MobileAlert.styles";
 
 const data = houses;
 
 export default function Home() {
+  const size = useWindowWidth();
   const [loaded, setLoaded] = useState<boolean>(false);
   const [isScrolling, setIsScrolling] = useState<boolean>(true);
   const [current, setCurrent] = useState<number>(0);
+  const [isMobile, setIsMobile] = useState<boolean>(size ? size <= 650 : false);
   const descriptionRefs = useRefArray<RefHandle>(data.length);
   const titleRefs = useRefArray<RefHandle>(data.length);
   const priceRefs = useRefArray<RefHandle>(data.length);
@@ -97,220 +104,245 @@ export default function Home() {
 
   useLayoutEffect(() => {
     const context = gsap.context((self) => {
-      const menu = self?.selector?.(".menu");
-      const headerLine = self?.selector?.(".header-line");
-      const firstDecriptionHeadingMask = self?.selector?.(
-        ".description-content:first-of-type h1 .heading-mask"
-      );
-      const firstDecriptionParagraph = self?.selector?.(
-        ".description-content:first-of-type p"
-      );
-      const firstTitle = self?.selector?.(".title-content:first-of-type");
-      const firstTitleChildren = self?.selector?.(
-        ".title-content:first-of-type > *"
-      );
-      const firstPrice = self?.selector?.(".price-content:first-of-type");
-      const firstNumberIndex = self?.selector?.(".number-index:first-of-type");
+      if (!isMobile) {
+        const menu = self?.selector?.(".menu");
+        const headerLine = self?.selector?.(".header-line");
+        const firstDecriptionHeadingMask = self?.selector?.(
+          ".description-content:first-of-type h1 .heading-mask"
+        );
+        const firstDecriptionParagraph = self?.selector?.(
+          ".description-content:first-of-type p"
+        );
+        const firstTitle = self?.selector?.(".title-content:first-of-type");
+        const firstTitleChildren = self?.selector?.(
+          ".title-content:first-of-type > *"
+        );
+        const firstPrice = self?.selector?.(".price-content:first-of-type");
+        const firstNumberIndex = self?.selector?.(
+          ".number-index:first-of-type"
+        );
 
-      gsap.set(backgroundOuterWrapperRef.current, { width: "100%" });
-      gsap.set(menu, { autoAlpha: 0, y: "10%" });
-      gsap.set(headerLine, {
-        scaleX: "0",
-        transformOrigin: "left",
-      });
-      gsap.set(firstTitle, {
-        scaleX: "0",
-        transformOrigin: "left",
-      });
-      gsap.set(priceWrapperRef.current, {
-        scaleX: "0",
-        transformOrigin: "left",
-      });
-      gsap.set(firstTitleChildren, { autoAlpha: 0, y: "1rem" });
-      gsap.set(firstPrice, { autoAlpha: 0, y: "1rem" });
-      gsap.set(firstDecriptionHeadingMask, {
-        scaleX: "1",
-        transformOrigin: "right",
-      });
-      gsap.set(firstDecriptionParagraph, { autoAlpha: 0, y: "1rem" });
-      gsap.set(numberWrapperRef.current, {
-        scaleX: "0",
-        transformOrigin: "left",
-      });
-      gsap.set(numberDividerRef.current, {
-        rotate: 0,
-        scale: 0,
-      });
-      gsap.set(totalNumberRef.current, { autoAlpha: 0, y: "40%" });
-      gsap.set(firstNumberIndex, { autoAlpha: 0, y: "-40%" });
-      const introAnimTimeline = gsap.timeline({ delay: 1.6 });
+        gsap.set(backgroundOuterWrapperRef.current, { width: "100%" });
+        gsap.set(menu, { autoAlpha: 0, y: "10%" });
+        gsap.set(headerLine, {
+          scaleX: "0",
+          transformOrigin: "left",
+        });
+        gsap.set(firstTitle, {
+          scaleX: "0",
+          transformOrigin: "left",
+        });
+        gsap.set(priceWrapperRef.current, {
+          scaleX: "0",
+          transformOrigin: "left",
+        });
+        gsap.set(firstTitleChildren, { autoAlpha: 0, y: "1rem" });
+        gsap.set(firstPrice, { autoAlpha: 0, y: "1rem" });
+        gsap.set(firstDecriptionHeadingMask, {
+          scaleX: "1",
+          transformOrigin: "right",
+        });
+        gsap.set(firstDecriptionParagraph, { autoAlpha: 0, y: "1rem" });
+        gsap.set(numberWrapperRef.current, {
+          scaleX: "0",
+          transformOrigin: "left",
+        });
+        gsap.set(numberDividerRef.current, {
+          rotate: 0,
+          scale: 0,
+        });
+        gsap.set(totalNumberRef.current, { autoAlpha: 0, y: "40%" });
+        gsap.set(firstNumberIndex, { autoAlpha: 0, y: "-40%" });
+        const introAnimTimeline = gsap.timeline({ delay: 1.6 });
 
-      introAnimTimeline
-        .to(backgroundOuterWrapperRef.current, {
-          width: "57%",
-          duration: 0.9,
-          ease: "expo.inOut",
-        })
-        .addLabel("firstLabel", 0.1)
-        .to(
-          firstTitle,
-          {
-            scaleX: "1",
-            duration: 0.4,
+        introAnimTimeline
+          .to(backgroundOuterWrapperRef.current, {
+            width: "57%",
+            duration: 0.9,
             ease: "expo.inOut",
-          },
-          "-=0.8"
-        )
-        .to(
-          priceWrapperRef.current,
-          {
-            scaleX: "1",
-            duration: 0.4,
-            ease: "expo.inOut",
-          },
-          "-=0.8"
-        )
-        .to(firstTitleChildren, { autoAlpha: 1, y: "0", duration: 0.3 })
-        .to(firstPrice, { autoAlpha: 1, y: "0", duration: 0.3 }, "-=0.3")
-        .to(
-          numberWrapperRef.current,
-          {
-            scaleX: 1,
+          })
+          .addLabel("firstLabel", 0.1)
+          .to(
+            firstTitle,
+            {
+              scaleX: "1",
+              duration: 0.4,
+              ease: "expo.inOut",
+            },
+            "-=0.8"
+          )
+          .to(
+            priceWrapperRef.current,
+            {
+              scaleX: "1",
+              duration: 0.4,
+              ease: "expo.inOut",
+            },
+            "-=0.8"
+          )
+          .to(firstTitleChildren, { autoAlpha: 1, y: "0", duration: 0.3 })
+          .to(firstPrice, { autoAlpha: 1, y: "0", duration: 0.3 }, "-=0.3")
+          .to(
+            numberWrapperRef.current,
+            {
+              scaleX: 1,
+              duration: 0.3,
+              ease: "expo.inOut",
+            },
+            "firstLabel"
+          )
+          .addLabel("secondLabel", 0.6)
+          .to(numberDividerRef.current, {
+            scale: 1,
             duration: 0.3,
             ease: "expo.inOut",
-          },
-          "firstLabel"
-        )
-        .addLabel("secondLabel", 0.6)
-        .to(numberDividerRef.current, {
-          scale: 1,
-          duration: 0.3,
-          ease: "expo.inOut",
-        })
-        .to(
-          numberDividerRef.current,
-          {
-            rotate: "25deg",
-            duration: 0.3,
-            ease: "expo.in",
-          },
-          "+=0.3"
-        )
-        .to(firstNumberIndex, { autoAlpha: 1, y: "0", duration: 0.2 })
-        .to(
-          totalNumberRef.current,
-          { autoAlpha: 1, y: "0", duration: 0.2 },
-          "-=0.2"
-        )
-        .to(
-          firstDecriptionHeadingMask,
-          {
-            scaleX: "0",
-            duration: 1,
-            ease: "expo.out",
-          },
-          "secondLabel"
-        )
-        .to(
-          firstDecriptionParagraph,
-          { autoAlpha: 1, y: "0", duration: 0.3 },
-          "secondLabel+=0.3"
-        )
-        .to(
-          headerLine,
-          {
-            scaleX: "1",
-            duration: 0.4,
-            ease: "expo.out",
-          },
-          "-=0.8"
-        )
-        .to(menu, { autoAlpha: 1, y: "0", duration: 0.4 }, "-=0.2");
+          })
+          .to(
+            numberDividerRef.current,
+            {
+              rotate: "25deg",
+              duration: 0.3,
+              ease: "expo.in",
+            },
+            "+=0.3"
+          )
+          .to(firstNumberIndex, { autoAlpha: 1, y: "0", duration: 0.2 })
+          .to(
+            totalNumberRef.current,
+            { autoAlpha: 1, y: "0", duration: 0.2 },
+            "-=0.2"
+          )
+          .to(
+            firstDecriptionHeadingMask,
+            {
+              scaleX: "0",
+              duration: 1,
+              ease: "expo.out",
+            },
+            "secondLabel"
+          )
+          .to(
+            firstDecriptionParagraph,
+            { autoAlpha: 1, y: "0", duration: 0.3 },
+            "secondLabel+=0.3"
+          )
+          .to(
+            headerLine,
+            {
+              scaleX: "1",
+              duration: 0.4,
+              ease: "expo.out",
+            },
+            "-=0.8"
+          )
+          .to(menu, { autoAlpha: 1, y: "0", duration: 0.4 }, "-=0.2");
 
-      introAnimTimeline.eventCallback("onComplete", () => {
-        setIsScrolling(false);
-      });
+        introAnimTimeline.eventCallback("onComplete", () => {
+          setIsScrolling(false);
+        });
+      }
     }, containerRef);
 
     return () => context.revert();
   }, []);
 
+  useEffect(() => {
+    if (size) {
+      setIsMobile(size <= 650);
+    }
+  }, [size]);
+
   return (
-    <div
-      ref={containerRef}
-      onWheel={handleScroll}
-      className={`${styles.container} ${montserrat.className}`}
-    >
-      <Header />
-      <StyledDescriptionContainer>
-        {data.map(({ mainTagline, secondaryTagline }, index) => (
-          <Description
-            ref={descriptionRefs[index]}
-            key={index}
-            dataCurrent={current}
-            dataIndex={index}
-            dataSubTilte={secondaryTagline}
-            dataTitle={mainTagline}
-          />
-        ))}
-      </StyledDescriptionContainer>
-      <StyledTitleAndPriceContainer>
-        <StyledTitleContainer ref={titleWrapperRef}>
-          {data.map(({ name, color, textColor }, index) => (
-            <Title
-              ref={titleRefs[index]}
-              key={index}
-              dataCurrent={current}
-              dataIndex={index}
-              dataTitle={name}
-              dataColor={color}
-              dataTextColor={textColor}
-            />
-          ))}
-        </StyledTitleContainer>
-        <StyledPriceContainer ref={priceWrapperRef}>
-          {data.map(({ priceInDollars, color }, index) => (
-            <Price
-              ref={priceRefs[index]}
-              key={index}
-              dataCurrent={current}
-              dataIndex={index}
-              dataPrice={priceInDollars}
-              dataColor={color}
-            />
-          ))}
-        </StyledPriceContainer>
-      </StyledTitleAndPriceContainer>
-      <StyledBackgroundImageOuterWrapper ref={backgroundOuterWrapperRef}>
-        <StyledBackgroundImageInnerWraper ref={backgroundWrapperRef}>
-          {data.map(({ img }, index) => (
-            <BackgroundImage
-              ref={backgroundRefs[index]}
-              dataCurrent={current}
-              dataIndex={index}
-              dataImage={img}
-              key={index}
-            />
-          ))}
-        </StyledBackgroundImageInnerWraper>
-      </StyledBackgroundImageOuterWrapper>
-      <StyledNumberWrapper ref={numberWrapperRef}>
-        <StyledNumberIndexWrapper>
-          {data.map((_, index) => (
-            <NumberIndex
-              ref={numberRefs[index]}
-              dataCurrent={current}
-              dataIndex={index}
-              key={index}
-            />
-          ))}
-        </StyledNumberIndexWrapper>
-        <StyledNumberDivider ref={numberDividerRef} />
-        <StyledTotalNumber ref={totalNumberRef}>
-          {data.length < 10 ? `0${data.length}` : data.length}
-        </StyledTotalNumber>
-      </StyledNumberWrapper>
-      {!loaded && <Preloader setLoaded={setLoaded} />}
+    <div>
+      {!isMobile && (
+        <div
+          ref={containerRef}
+          onWheel={handleScroll}
+          className={`${styles.container} ${montserrat.className}`}
+        >
+          <Header />
+          <StyledDescriptionContainer>
+            {data.map(({ mainTagline, secondaryTagline }, index) => (
+              <Description
+                ref={descriptionRefs[index]}
+                key={index}
+                dataCurrent={current}
+                dataIndex={index}
+                dataSubTilte={secondaryTagline}
+                dataTitle={mainTagline}
+              />
+            ))}
+          </StyledDescriptionContainer>
+          <StyledTitleAndPriceContainer>
+            <StyledTitleContainer ref={titleWrapperRef}>
+              {data.map(({ name, color, textColor }, index) => (
+                <Title
+                  ref={titleRefs[index]}
+                  key={index}
+                  dataCurrent={current}
+                  dataIndex={index}
+                  dataTitle={name}
+                  dataColor={color}
+                  dataTextColor={textColor}
+                />
+              ))}
+            </StyledTitleContainer>
+            <StyledPriceContainer ref={priceWrapperRef}>
+              {data.map(({ priceInDollars, color }, index) => (
+                <Price
+                  ref={priceRefs[index]}
+                  key={index}
+                  dataCurrent={current}
+                  dataIndex={index}
+                  dataPrice={priceInDollars}
+                  dataColor={color}
+                />
+              ))}
+            </StyledPriceContainer>
+          </StyledTitleAndPriceContainer>
+          <StyledBackgroundImageOuterWrapper ref={backgroundOuterWrapperRef}>
+            <StyledBackgroundImageInnerWraper ref={backgroundWrapperRef}>
+              {data.map(({ img }, index) => (
+                <BackgroundImage
+                  ref={backgroundRefs[index]}
+                  dataCurrent={current}
+                  dataIndex={index}
+                  dataImage={img}
+                  key={index}
+                />
+              ))}
+            </StyledBackgroundImageInnerWraper>
+          </StyledBackgroundImageOuterWrapper>
+          <StyledNumberWrapper ref={numberWrapperRef}>
+            <StyledNumberIndexWrapper>
+              {data.map((_, index) => (
+                <NumberIndex
+                  ref={numberRefs[index]}
+                  dataCurrent={current}
+                  dataIndex={index}
+                  key={index}
+                />
+              ))}
+            </StyledNumberIndexWrapper>
+            <StyledNumberDivider ref={numberDividerRef} />
+            <StyledTotalNumber ref={totalNumberRef}>
+              {data.length < 10 ? `0${data.length}` : data.length}
+            </StyledTotalNumber>
+          </StyledNumberWrapper>
+          {!loaded && <Preloader setLoaded={setLoaded} />}
+        </div>
+      )}
+      {isMobile && (
+        <StyledMobileAlertContainer>
+          <StyledMobileAlertText>
+            No mobile boss.
+            <br />
+            Open your laptop 👍🏿
+            <br />
+            <span>(or use desktop mode)</span>
+          </StyledMobileAlertText>
+        </StyledMobileAlertContainer>
+      )}
     </div>
   );
 }
---
+++ components/MobileAlert/MobileAlert.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledMobileAlertContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledMobileAlertText = styled.h1`
+  font-size: 6rem;
+  padding: 45% 13%;
+  span {
+    font-size: 4rem;
+    font-weight: 400;
+  }
+`;
